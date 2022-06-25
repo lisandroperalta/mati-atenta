1e861cb7a23b5db18f2aa4a1d3d5dd3340a39819
Lisandro Peralta <lisandroperalta@gmail.com>
2022-06-25 16:54:46 -0300

    dedo,pausa,scroll, noise
    arregle el dedo
    pausa cuando minimizo
    evito el refresh con el scroll
    agregue noise para mover el feedback en x, y, y escala

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let miFuente;
 let misPinceles = [];
 let misTexturasPinceles = [];
 
-let  ratioDeEscala= 1;
+let ratioDeEscala = 1;
 
 
 ///////////////////////////////////////77 PRELOAD
@@ -34,8 +34,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   buffer1 = createGraphics(windowWidth, windowHeight, WEBGL);
   buffer2 = createGraphics(windowWidth, windowHeight, WEBGL);
-ratioDeEscala = windowWidth / 1500;
-  
+  ratioDeEscala = windowWidth / 1920;
+
   tamanioFuente = windowWidth / 20;
   button = createButton("play/stop");
   button.position(10, 10);
@@ -62,16 +62,16 @@ function draw() {
   //dibujarLinea();
   buffer1.push();
   buffer1.noStroke(0);
-  buffer1.blendMode(ADD);
   buffer1.translate(0, 0, 0);
   buffer1.texture(buffer2); // meto la textura de salida en la entrada
   buffer1.plane(windowWidth, windowHeight); // dibujo la textura de salida en la entrada  
 
   //hago todo lo que quiero hacer
+  buffer1.translate(-windowWidth / 2, -windowHeight / 2); //CORRIJO TRANSLATE DE WEBGL
 
   dibujarDedo();
   buffer1.noStroke();
-  buffer1.blendMode(ADD);
+  buffer1.blendMode(DIFFERENCE);
   for (let i = 0; i < misPinceles.length; i++) {
     //misPinceles[i].mover();
     misPinceles[i].moverNoise();
@@ -86,15 +86,24 @@ function draw() {
   //meto lo que hice en la imagen de salida para reusarla despues
   buffer2.push();
   buffer2.noStroke();
-  buffer2.scale(1.01, 1.01); //hago transformaciones
+
+  let noiseTranslateX = map(noise(frameCount * 0.005 + 150), 0, 1, -5, 5);
+  let noiseTranslateY = map(noise(frameCount * 0.005), 0, 1, -5, 5);
+  let noiseScale = map(noise(frameCount * 0.005+750), 0, 1, 0.999, 1.01);
+
+  buffer2.translate(noiseTranslateX, noiseTranslateY); //hago transformaciones
+
+  //buffer2.rotate(radians(0.2));
   buffer2.texture(buffer1); //meto la textura de salida
-  
+  buffer2.scale(noiseScale); //hago transformaciones
   buffer2.plane(windowWidth, windowHeight); //dibujo la textura de salida
   buffer2.pop();
 
   image(buffer2, 0, 0, windowWidth, windowHeight);
 
-
+  if (!focused) {
+    mySound.pause();
+  }
 
 }
 ////////////////////////objeto pincel
@@ -104,25 +113,25 @@ class Pincel {
 
   constructor() {
 
-this.reiniciarPincel ();
+    this.reiniciarPincel();
   }
-reiniciarPincel (){
-  var margen= 100* ratioDeEscala;
-  this.x = random(0+margen, windowWidth-margen);
-  this.y = random(0+margen*2, windowHeight-margen*2);
-  this.posZ = random(0.0, -1)
-  this.posicionAnteriorX = this.x;
-  this.posicionAnteriorY = this.y;
-  this.xoffset = random(10);
-  this.yoffset = random(10);
-  this.velocidadOffsetx = random(0.001, 0.0015);
-  this.velocidadOffsety = random(0.001, 0.0015);
-  this.posicion = 0;
+  reiniciarPincel() {
+    var margen = 200 * ratioDeEscala;
+    this.x = random(0 + margen, windowWidth - margen);
+    this.y = random(0 + margen * 4, windowHeight - margen * 4);
+    this.posZ = random(0.0, -1)
+    this.posicionAnteriorX = this.x;
+    this.posicionAnteriorY = this.y;
+    this.xoffset = random(10);
+    this.yoffset = random(10);
+    this.velocidadOffsetx = random(0.001, 0.0015);
+    this.velocidadOffsety = random(0.001, 0.0015);
+    this.posicion = 0;
 
 
 
 
-}
+  }
   moverNoise() {
     this.x += map(noise(this.xoffset), 0, 1, -0.5, 0.5);
     this.xoffset += this.velocidadOffsetx;
@@ -134,12 +143,11 @@ reiniciarPincel (){
   dibujar3d(i) {
     buffer1.push();
 
-    buffer1.translate(-windowWidth / 2, -windowHeight / 2);
 
     buffer1.translate(this.x, this.y);
     buffer1.texture(misTexturasPinceles[i]);
 
-    buffer1.plane(misTexturasPinceles[i].width*ratioDeEscala, misTexturasPinceles[i].height*ratioDeEscala, 150, 150);
+    buffer1.plane(misTexturasPinceles[i].width * ratioDeEscala, misTexturasPinceles[i].height * ratioDeEscala, 150, 150);
     buffer1.pop();
   }
 
@@ -149,21 +157,19 @@ reiniciarPincel (){
 
 //// dibujar linea
 
-
 function dibujarDedo() {
   if (mouseX != pmouseX || mouseY != pmouseY) {
     push();
     imageMode(CENTER);
     buffer1.tint(255);
-
-    buffer1.image(miDedo, mouseX - width / 2, mouseY - height / 2, 100, 100);
-    buffer1.tint(255);
-
+    let escalaDedo = ratioDeEscala * 500;
+    buffer1.image(miDedo, mouseX - escalaDedo / 2, mouseY - escalaDedo / 2, escalaDedo, escalaDedo);
     pop();
 
   }
 
 }
+
 
 function dibujarLinea() {
   if (mouseX != pmouseX || mouseY != pmouseY) {
@@ -191,12 +197,12 @@ function dibujarTexto3D() {
   buffer1.stroke(0);
   buffer1.strokeWeight(2);
   buffer1.fill(sin(frameCount * 0.015) * 255, sin(frameCount * 0.02) * 255, sin(frameCount * 0.011) * 255);
-  buffer1.textSize(windowWidth / 20);
-  buffer1.tamanioFuente = windowWidth / 20;
+  buffer1.textSize(90);
+  // buffer1.tamanioFuente = 500;
   buffer1.textFont(miFuente);
-
-  //translate(0,0);
-  buffer1.text(windowWidth, 0, 0);
+  buffer1.translate(windowWidth / 2, windowHeight / 2)
+  translate(0, 0);
+  buffer1.text(ratioDeEscala, 0, 0);
   pop();
 }
 
@@ -243,10 +249,13 @@ function dibujarTexto() {
 /////////// extras
 function reproducir() {
   if (mySound.isPlaying()) {
+    mySound.
     mySound.stop();
   } else {
     mySound.play();
   }
+
+
   background(0);
 
 }
@@ -259,7 +268,7 @@ function windowResized() {
   for (let i = 0; i < misPinceles.length; i++) {
     //misPinceles[i].mover();
     misPinceles[i].reiniciarPincel();
- 
+
   }
-ratioDeEscala = windowWidth / 1500;
+  ratioDeEscala = windowWidth / 1920;
 }
